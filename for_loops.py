8234a71a0fc88b678758a246824bd9870f39df34
Sezer Garip <sezer1994@hotmail.co.uk>
2019-04-17 14:45:54 +0100

    Start of lecture with for loop and embedded data

---
+++ for_loops.py
@@ -0,0 +1,15 @@
+# movies = ["Avengers", "Spider man", "Thor", "Rick And Morty"]
+# counter = 0
+#
+# for id in movies:
+#     print(counter +1, "-", id)
+#     counter += 1
+#
+
+
+embedded_data = [[1, 2, 3], [7, 12, 1], ["Skoda Feceilia Fun", "Panda 4*4"]]
+
+for data_list in embedded_data:
+    print(data_list)
+    for data in data_list:
+        print(data)
